e28e9533cc4973cb6b78e6c3b10ba74cee626b61
Goswami, Murari <murari_goswami@hotmail.com>
2018-05-11 13:10:19 +0200

    first commit

---
+++ src/main/java/com/adidas/bootstrap/blitz/BlitzApplication.java
@@ -0,0 +1,12 @@
+package com.adidas.bootstrap.blitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlitzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlitzApplication.class, args);
+	}
+}
